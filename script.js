f38f723c2a0f55dee2679a7e37f409d93f391983
Prasadk1226 <124857435+Prasadk1226@users.noreply.github.com>
2024-09-20 15:49:45 +0530

    changed

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let next=document.getAnimations("next")
 
 
 page.addEventListener("click",()=>{
-    // location.assign("https://github.com/Prasadk1226")
-    window.open("https://github.com/Prasadk1226")
+    location.assign("https://github.com/Prasadk1226")
+    // window.open("https://github.com/Prasadk1226")
 })
 
 page1.addEventListener("click",()=>{
-    window.open("https://prasadk1226.github.io/BOM-in-JavaScript/")
+    location.assign("https://prasadk1226.github.io/BOM-in-JavaScript/")
 })
 
 close.addEventListener("click",()=>{
