5286785dac8c1804eca56ce1741e66874f95b0d9
Prasadk1226 <124857435+Prasadk1226@users.noreply.github.com>
2024-09-20 15:15:50 +0530

    BOM

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let page=document.getElementById("page")
+let page1=document.getElementById("page1")
+let page2=document.getAnimations("page2")
+let prev=document.getAnimations("prev")
+let next=document.getAnimations("next")
+
+
+page.addEventListener("click",()=>{
+    // location.assign("https://github.com/Prasadk1226")
+    window.open("https://github.com/Prasadk1226")
+})
+
+
+
+
+
+
+
+// window.alert("Hey Hi! Welcome.")
+// let wish= window.prompt("Enter your Name")
+// window.confirm("Is your  name "+wish+"?")
+// alert("Hello "+wish+"!")
+
+
